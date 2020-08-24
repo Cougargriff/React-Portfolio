ffff55982be86f16fa6d2bc1ea4f058139e5584f
Griffin Johnson <cougargriff@gmail.com>
2020-08-24 12:52:40 -0700

    cruD
    deletePost when admin works

--- portfolio-app/src/components/Posts.js
+++ portfolio-app/src/components/Posts.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import styled from 'styled-components';
-import { fetchPosts } from '../store/actions/PostActions';
+import { fetchPosts, deletePost } from '../store/actions/PostActions';
 import { Title } from './TextBox';
 import ReactMarkdown from 'react-markdown'
+import { NColumnContent } from './Containers';
+import ColorButton from "./ColorButton";
 
 const PostsContainer = styled.div`
   width: 100%;
@@ -30,6 +32,8 @@ function Posts (props) {
   const postsError = useSelector(state => state.PostsReducer.fetchPostsError)
   const postsFetched = useSelector(state => state.PostsReducer.fetchedPosts)
   const posts = useSelector(state => state.PostsReducer.posts)
+  const isAdmin = useSelector((state) => state.EditorReducer.isAdmin);
+
 
   useEffect(() => {
     // Run! Like go get some data from an API.
@@ -45,6 +49,15 @@ function Posts (props) {
           return (
             <PostContainer key={i} >
               <Post >
+                {isAdmin ? 
+                <ColorButton  
+                color='#FF0000'
+                text='Delete'
+                onClick={() => {
+                  dispatch(deletePost(post.id))
+                }}
+                />: undefined}
+              
                 <Title>
                   {post.title}
                 </Title>
