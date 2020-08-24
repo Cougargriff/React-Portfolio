45f591df946271a4f9f18a398e4849bcba797ecc
Griffin Johnson <cougargriff@gmail.com>
2020-08-23 19:29:13 -0700

    added firebase config

---
+++ portfolio-app/src/Firebase.js
@@ -0,0 +1,20 @@
+/*
+  Config file for firebase.
+  Used for auth
+*/
+
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFJrBNnIRUICA_9USBeEUXoyLKO9AyBxk",
+  authDomain: "portfolio-df99c.firebaseapp.com",
+  databaseURL: "https://portfolio-df99c.firebaseio.com",
+  projectId: "portfolio-df99c",
+  storageBucket: "portfolio-df99c.appspot.com",
+  messagingSenderId: "184582598071",
+  appId: "1:184582598071:web:dfd8993f99e9ee8d70cc32"
+};
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
