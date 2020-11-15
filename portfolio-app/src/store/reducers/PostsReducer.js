4fa4d4ceb9eed8cd4039087e10be40d7ac20348a
Griffin <cougargriff@gmail.com>
2020-11-15 10:26:46 -0800

    update

--- portfolio-app/src/store/reducers/PostsReducer.js
+++ portfolio-app/src/store/reducers/PostsReducer.js
@@ -28,11 +28,11 @@ const initPostsState = {
 const postsReducer = (state = initPostsState, action) => {
   switch (action.type) {
     /*
-            On each parsed action request type, the reducer will return the
-            new modified state. The spread operator '...' will return the insides of the old
-            state which we then append our corresponding changed state values to. Altogether returning
-            a new state object. 
-        */
+      On each parsed action request type, the reducer will return the
+      new modified state. The spread operator '...' will return the insides of the old
+      state which we then append our corresponding changed state values to. Altogether returning
+      a new state object. 
+    */
     case CLEAR_EDIT_ID:
       return {
         ...state,
@@ -80,7 +80,6 @@ const postsReducer = (state = initPostsState, action) => {
         fetchPostError: true,
       };
     case FETCH_POST_SUCCESS:
-      console.log(action.post);
       return {
         ...state,
         isFetchingPost: false,
